63cea82ee0964f0d67de5d9c78b72363a28a4d90
ramarojaona <144231899+Yves-MG@users.noreply.github.com>
2025-02-26 21:01:25 +0100

    first push

---
+++ Movie.Api/Program.cs
@@ -0,0 +1,35 @@
+using Movie.Api.Dtos;
+using Microsoft.AspNetCore.Http;
+
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+List<MovieDto> movies = [
+    new(1,"malokila","yves",new DateOnly(2014,02,14)),
+    new(2,"malokila","Manoela",new DateOnly(2014,02,14)),
+    new(3,"malokila","Mirindra",new DateOnly(2014,02,14)),
+    new(4,"malokila","Mirindra",new DateOnly(2014,02,14))
+
+    ];
+
+app.MapGet("/Movie/list", () => movies).WithName("ListMovies");
+app.MapGet("/Movie/{id}", (int id ) => movies.Find(x=>x.Id==id))
+   .WithName("List");
+app.MapPost("/Movie/", (CreateMovieDto createMovieDto) =>
+{
+    MovieDto movieDto = new
+    (
+        Id: movies.Count() + 1,
+        Title: createMovieDto.Title,
+        Director: createMovieDto.Director,
+        ReleaseDate: new DateOnly(2024, 12, 14)
+    );
+
+    movies.Add(movieDto);
+
+    return Results.CreatedAtRoute("ListMovies",new {},movies);
+    //return Results.CreatedAtRoute("ListMovies", new { id  = movieDto.Id},movieDto);
+});
+
+app.MapGet("/", () => "Hello World!");
+
+app.Run();
